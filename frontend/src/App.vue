dcfdf704c6f1bf9051398fbb820f31cd218bf935
YusukeYoshida23 <not_over_yet.1123@hotmail.co.jp>
2024-10-15 08:36:58 +0900

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Vue + Laravel API Test</h1>
+    <ApiTest />
+  </div>
+</template>
+
+<script>
+import ApiTest from './components/ApiTest.vue'
+
+export default {
+  components: {
+    ApiTest
+  }
+}
+</script>
---
+++ frontend/src/components/ApiTest.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>API Test</h2>
+    <button @click="fetchData">Test API</button>
+    <p v-if="message">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get('http://localhost:8000/api/test')
+        this.message = response.data.message
+      } catch (error) {
+        console.error('Error:', error)
+        this.message = 'Error occurred while fetching data'
+      }
+    }
+  }
+}
+</script>
